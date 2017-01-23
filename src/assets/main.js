4460a0945e3c276d0c1a4bb846ff81bc0928dab9
Dominika <kumkwaczka@gmail.com>
2017-01-23 21:06:09 +0100

    adding my code

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
-  // your code will go here
+	$.ajax({
+		url: 'https://www.codeschool.com/users/yterais.json',
+		dataType: 'jsonp',
+		success: function(response) {
+			allCourses(response.courses.completed);
+		}
+	});
 
+	function allCourses(courses) {
+		var $badges = $('#badges');
+		courses.forEach(function(course) {
+			var $course = $('<div />', {'class': 'course'}).appendTo($badges);
+			$('<h3 />', {text: course.title}).appendTo($course);
+			$('<img />', {src: course.badge,}).appendTo($course);
+			$('<a />', {'class': 'btn btn-primary', target: '_blank', href: course.url, text: 'See Course'}).appendTo($course);
+			})
+	}
 });
